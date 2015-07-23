74be623273011d094110fcec7be041ee47ccdbf2
David A. Parker <parker.david.a@gmail.com>
2015-07-23 09:53:22 -0400

    Support old GoldSrc (HLTV) response
    Changed to use ByteBuffers
    
    Replaced generic Exception catch with addErrorRow()

--- src/com/dparker/apps/checkvalve/ServerQuery.java
+++ src/com/dparker/apps/checkvalve/ServerQuery.java
@@ -19,6 +19,7 @@
 
 package com.dparker.apps.checkvalve;
 
+import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.util.TypedValue;
@@ -27,7 +28,10 @@ import android.widget.TableRow;
 import android.widget.TableRow.LayoutParams;
 import android.content.Context;
 import android.graphics.Typeface;
+import java.io.UnsupportedEncodingException;
 import java.net.*;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import com.dparker.apps.checkvalve.R;
 
 public class ServerQuery implements Runnable {
@@ -37,6 +41,7 @@ public class ServerQuery implements Runnable {
     private TableRow[][] tableRows;
     private TableRow[] messageRows;
     private int status;
+    private int m;
 
     private static final String TAG = ServerQuery.class.getSimpleName();
 
@@ -54,6 +59,7 @@ public class ServerQuery implements Runnable {
         this.context = c;
         this.status = 0;
         this.handler = h;
+        this.m = 0;
     }
 
     public void run() {
@@ -62,18 +68,43 @@ public class ServerQuery implements Runnable {
         status = 0;
 
         Log.d(TAG, "Calling queryServers()");
-        queryServers();
+        
+        try {
+            queryServers();
+        }
+        catch( Exception e ) {
+            Log.w(TAG, "run(): Caught an exception:");
+            Log.w(TAG, e.toString());
+            
+            StackTraceElement[] ste = e.getStackTrace();
+            
+            for( StackTraceElement x : ste )
+                Log.w(TAG, "    " + x.toString());
+            
+            status = -1;
+        }
 
-        if( handler != null )
-            this.handler.sendEmptyMessage(status);
+        if( handler != null ) this.handler.sendEmptyMessage(status);
     }
 
-    public void queryServers() {
+    public void queryServers() throws UnsupportedEncodingException {
         // Get the server list from the database
         DatabaseProvider database = new DatabaseProvider(context);
         ServerRecord[] serverList = database.getAllServers();
         database.close();
+        
+        m = 0;
 
+        // The outgoing data only needs to be set up once
+        byte[] arrayOut = new byte[25];
+        ByteBuffer bufferOut = ByteBuffer.wrap(arrayOut);
+        
+        bufferOut.order(ByteOrder.BIG_ENDIAN);        
+        bufferOut.putInt(Values.INT_PACKET_HEADER);
+        bufferOut.put(Values.BYTE_A2S_INFO);
+        bufferOut.put(Values.A2S_INFO_QUERY.getBytes("UTF-8"));
+        bufferOut.put((byte)0x00);
+                
         // Allocate the TableRow array
         if( tableRows == null )
             tableRows = new TableRow[serverList.length][6];
@@ -82,67 +113,40 @@ public class ServerQuery implements Runnable {
         for( int i = 0; i < messageRows.length; i++ )
             messageRows[i] = null;
 
-        // String variables
-        String serverURL = new String();
-        String serverName = new String();
-        String serverIP = new String();
-        String serverMap = new String();
-        String serverGame = new String();
-        String gameVersion = new String();
-        String serverTags = new String();
-
-        // Integer variables
-        int byteNum = 0;
-        int serverPort = 0;
-        int serverTimeout = 0;
-        int serverNumPlayers = 0;
-        int serverMaxPlayers = 0;
-        int serverRowId = 0;
-        int serverListPos = 0;
-
-        int m = 0;
-
         for( int i = 0; i < serverList.length; i++ ) {
             ServerRecord sr = serverList[i];
+            Bundle response = new Bundle();
 
+            String serverURL = sr.getServerName();
+            int serverRowId = (int)sr.getServerRowID();
+            int serverPort = sr.getServerPort();
+            int serverTimeout = sr.getServerTimeout();
+            int serverListPos = sr.getServerListPosition();
+            
             try {
                 socket = new DatagramSocket();
-
-                // String variables
-                serverURL = new String();
-                serverName = new String();
-                serverIP = new String();
-                serverMap = new String();
-                serverGame = new String();
-                gameVersion = new String();
-                serverTags = new String();
-
-                serverRowId = (int)sr.getServerRowID();
-                serverURL = sr.getServerName();
-                serverPort = sr.getServerPort();
-                serverTimeout = sr.getServerTimeout();
-                serverListPos = sr.getServerListPosition();
-
-                // A2S_INFO query string
-                String queryString = "\u00FF\u00FF\u00FF\u00FF\u0054Source Engine Query\0";
-
                 socket.setSoTimeout(serverTimeout * 1000);
 
                 // Byte buffers for packet data
-                byte[] bufferOut = queryString.getBytes("ISO8859_1");
-                byte[] bufferIn = new byte[1400];
-
+                byte[] arrayIn = new byte[1400];
+                
+                ByteBuffer bufferIn = ByteBuffer.wrap(arrayIn);
+                bufferIn.order(ByteOrder.BIG_ENDIAN);
+                
                 // UDP datagram packets
-                DatagramPacket packetOut = new DatagramPacket(bufferOut, bufferOut.length);
-                DatagramPacket packetIn = new DatagramPacket(bufferIn, bufferIn.length);
+                DatagramPacket packetOut = new DatagramPacket(arrayOut, arrayOut.length);
+                DatagramPacket packetIn = new DatagramPacket(arrayIn, arrayIn.length);
 
                 // Connect to the remote server
                 socket.connect(InetAddress.getByName(serverURL), serverPort);
 
                 // Show an error if the connection attempt failed
-                if( !socket.isConnected() ) throw new Exception();
+                if( !socket.isConnected() ) {
+                    tableRows[i] = null;
+                    addErrorRow(serverURL, serverPort, serverListPos);
+                }
 
-                serverIP = socket.getInetAddress().getHostAddress();
+                String serverIP = socket.getInetAddress().getHostAddress();
 
                 // Send the query string to the server
                 socket.send(packetOut);
@@ -150,82 +154,46 @@ public class ServerQuery implements Runnable {
                 // Receive the response packet from the server
                 socket.receive(packetIn);
 
-                // Start at byte 6 in the response data
-                byteNum = 6;
-
-                // Get the server name
-                while( bufferIn[byteNum] != 0x00 )
-                    serverName += (char)bufferIn[byteNum++];
-
-                byteNum++;
-
-                // Get the map name
-                while( bufferIn[byteNum] != 0x00 )
-                    serverMap += (char)bufferIn[byteNum++];
-
-                byteNum++;
-
-                // Skip the next string (game server path)
-                while( bufferIn[byteNum] != 0x00 )
-                    byteNum++;
-
-                byteNum++;
-
-                // Get the game description
-                while( bufferIn[byteNum] != 0x00 )
-                    serverGame += (char)bufferIn[byteNum++];
-
-                byteNum++;
-
-                // Skip the next 2 bytes (Steam application ID)
-                byteNum += 2;
-
-                // Get the current number of players and move to the next byte
-                serverNumPlayers = (int)bufferIn[byteNum++];
-
-                // Get the maximum number of players and move to the next byte
-                serverMaxPlayers = (int)bufferIn[byteNum++];
-
-                byteNum += 5;
-
-                while( bufferIn[byteNum] != 0x00 )
-                    gameVersion += (char)bufferIn[byteNum++];
-
-                byteNum++;
-
-                // If we're not at the end of the array then get the additional data
-                if( byteNum < bufferIn.length ) {
-                    // This byte is the Extra Data Flag (EDF)
-                    int EDF = (int)bufferIn[byteNum];
-                    byteNum++;
-
-                    // Skip the port number if included
-                    if( (EDF & 0x80) > 0 ) byteNum += 2;
-
-                    // Skip the SteamID if included
-                    if( (EDF & 0x10) > 0 ) byteNum += 8;
-
-                    // Skip SourceTV information if included
-                    if( (EDF & 0x40) > 0 ) {
-                        byteNum += 2;
-
-                        while( bufferIn[byteNum] != 0x00 )
-                            byteNum++;
-
-                        byteNum++;
-                    }
-
-                    // Get the server tags (sv_tags) if any are included
-                    if( (EDF & 0x20) > 0 ) while( bufferIn[byteNum] != 0 )
-                        serverTags += (char)bufferIn[byteNum++];
-
-                    /*
-                     * Stop here (we're only interested in getting the server tags in this query)
-                     */
+                int packetHeader = bufferIn.getInt();
+                
+                // Make sure the packet includes the expected header bytes
+                if( packetHeader != 0xFFFFFFFF ) {
+                    String rcv = "0x" + String.format("%8s", Integer.toHexString(packetHeader)).replace(' ','0').toUpperCase();
+                    Log.w(TAG, "Packet header " + rcv + " does not match expected value 0xFFFFFFFF");
+                    tableRows[i] = null;
+                    addErrorRow(serverURL, serverPort, serverListPos);
                 }
-
+                
+                byte packetType = bufferIn.get();
+                
+                if( packetType == 0x49 ) {
+                    // Parse response in the Source (and newer GoldSrc) format
+                    Log.i(TAG, "Parsing Source Engine response");
+                    response = parseResponseFromSRCDS(arrayIn);
+                }
+                else if( packetType == 0x6d ) {
+                    // Parse response in the old GoldSrc format
+                    Log.i(TAG, "Parsing GoldSrc Engine response");
+                    response = parseResponseFromHLDS(arrayIn);
+                }
+                else {
+                    // Packet type did not match 0x49 or 0x6D
+                    String rcv = "0x" + String.format("%2s", Byte.toString(packetType)).replace(' ','0').toUpperCase();
+                    Log.w(TAG, "Packet type " + rcv + " does not match expected values 0x49 or 0x6d");
+                    tableRows[i] = null;
+                    addErrorRow(serverURL, serverPort, serverListPos);
+                }
+                
                 // Close the UDP socket
                 socket.close();
+                
+                String serverName = response.getString(Values.SERVER_NAME);
+                String serverMap = response.getString(Values.SERVER_MAP);
+                String serverGame = response.getString(Values.SERVER_GAME);
+                String gameVersion = response.getString(Values.SERVER_VERSION);
+                String serverTags = response.getString(Values.SERVER_TAGS);
+                int serverNumPlayers = response.getInt(Values.SERVER_NUM_PLAYERS);
+                int serverMaxPlayers = response.getInt(Values.SERVER_MAX_PLAYERS);
 
                 TextView serverLabel = new TextView(context);
                 TextView serverValue = new TextView(context);
@@ -280,10 +248,14 @@ public class ServerQuery implements Runnable {
                 gameLabel.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
 
                 gameValue.setId(i * 700);
-                gameValue.setText(serverGame + " [" + gameVersion + "]");
                 gameValue.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 12.0f);
                 gameValue.setPadding(3, 0, 3, 0);
                 gameValue.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+
+                if( gameVersion.length() > 0 )
+                    gameValue.setText(serverGame + " [" + gameVersion + "]");
+                else
+                    gameValue.setText(serverGame);
 
                 mapLabel.setId(i * 800);
                 mapLabel.setText(context.getText(R.string.label_map));
@@ -384,38 +356,172 @@ public class ServerQuery implements Runnable {
                 tableRows[i][3] = mapRow;
                 tableRows[i][4] = playersRow;
                 tableRows[i][5] = tagsRow;
-
             }
-            // Handle a socket timeout
             catch( Exception e ) {
                 tableRows[i] = null;
-
-                String message = new String();
-
-                message += context.getText(R.string.msg_no_response);
-                message += " " + serverURL + ":" + serverPort;
-
-                TextView errorMessage = new TextView(context);
-
-                //errorMessage.setId(100 + i + 2);
-                errorMessage.setId(100 + serverListPos + 2);
-                errorMessage.setText(message);
-                errorMessage.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 12.0f);
-                errorMessage.setPadding(3, 0, 3, 0);
-                errorMessage.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
-
-                // Create a TableRow and give it an ID
-                TableRow messageRow = new TableRow(context);
-                messageRow.setId(0);
-                messageRow.setBackgroundResource(R.color.translucent_red);
-                messageRow.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
-
-                messageRow.addView(errorMessage);
-
-                messageRows[m++] = messageRow;
+                addErrorRow(serverURL, serverPort, serverListPos);
             }
 
             status++;
         }
+    }
+    
+    public void addErrorRow( String server, int port, int pos ) {
+        String message = new String();
+
+        message += context.getText(R.string.msg_no_response);
+        message += " " + server + ":" + port;
+
+        TextView errorMessage = new TextView(context);
+
+        errorMessage.setId(100 + pos + 2);
+        errorMessage.setText(message);
+        errorMessage.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 12.0f);
+        errorMessage.setPadding(3, 0, 3, 0);
+        errorMessage.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+
+        // Create a TableRow and give it an ID
+        TableRow messageRow = new TableRow(context);
+        messageRow.setId(0);
+        messageRow.setBackgroundResource(R.color.translucent_red);
+        messageRow.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
+        messageRow.addView(errorMessage);
+
+        messageRows[m++] = messageRow;
+    }
+    
+    public Bundle parseResponseFromSRCDS(byte[] bufferIn) {
+        Bundle result = new Bundle();
+
+        String name = new String();
+        String map = new String();
+        String game = new String();
+        String version = new String();
+        String tags = new String();
+        int numPlayers = 0;
+        int maxPlayers = 0;
+        
+        // Start at byte 6 in the response data
+        int byteNum = 6;
+
+        // Get the server name
+        while( bufferIn[byteNum] != 0x00 ) name += (char)bufferIn[byteNum++];
+        byteNum++;
+
+        // Get the map name
+        while( bufferIn[byteNum] != 0x00 ) map += (char)bufferIn[byteNum++];
+        byteNum++;
+
+        // Skip the next string (game server path)
+        while( bufferIn[byteNum] != 0x00 ) byteNum++;
+        byteNum++;
+
+        // Get the game description
+        while( bufferIn[byteNum] != 0x00 ) game += (char)bufferIn[byteNum++];
+        byteNum++;
+
+        // Skip the next 2 bytes (Steam application ID)
+        byteNum += 2;
+
+        // Get the current number of players and move to the next byte
+        numPlayers = (int)bufferIn[byteNum++];
+
+        // Get the maximum number of players and move to the next byte
+        maxPlayers = (int)bufferIn[byteNum++];
+
+        byteNum += 5;
+
+        while( bufferIn[byteNum] != 0x00 ) version += (char)bufferIn[byteNum++];
+        byteNum++;
+
+        // If we're not at the end of the array then get the additional data
+        if( byteNum < bufferIn.length ) {
+            // This byte is the Extra Data Flag (EDF)
+            int EDF = (int)bufferIn[byteNum];
+            byteNum++;
+
+            // Skip the port number if included
+            if( (EDF & 0x80) > 0 ) byteNum += 2;
+
+            // Skip the SteamID if included
+            if( (EDF & 0x10) > 0 ) byteNum += 8;
+
+            // Skip SourceTV information if included
+            if( (EDF & 0x40) > 0 ) {
+                byteNum += 2;
+
+                while( bufferIn[byteNum] != 0x00 ) byteNum++;
+                byteNum++;
+            }
+
+            // Get the server tags (sv_tags) if any are included
+            if( (EDF & 0x20) > 0 ) while( bufferIn[byteNum] != 0 )
+                tags += (char)bufferIn[byteNum++];
+
+            /*
+             * Stop here (we're only interested in getting the server tags in this query)
+             */
+        }
+        
+        result.putString(Values.SERVER_NAME, name);
+        result.putString(Values.SERVER_MAP, map);
+        result.putString(Values.SERVER_GAME, game);
+        result.putString(Values.SERVER_VERSION, version);
+        result.putString(Values.SERVER_TAGS, tags);
+        result.putInt(Values.SERVER_NUM_PLAYERS, numPlayers);
+        result.putInt(Values.SERVER_MAX_PLAYERS, maxPlayers);
+        
+        return result;
+    }
+    
+    public Bundle parseResponseFromHLDS(byte[] bufferIn) {
+        Bundle result = new Bundle();
+
+        String name = new String();
+        String map = new String();
+        String game = new String();
+        String version = new String();
+        String tags = new String();
+        int numPlayers = 0;
+        int maxPlayers = 0;
+        
+        // Start at byte 5 in the response data
+        int byteNum = 5;
+
+        // Skip the server IP
+        while( bufferIn[byteNum] != 0x00 ) byteNum++;
+        byteNum++;
+        
+        // Get the server name
+        while( bufferIn[byteNum] != 0x00 ) name += (char)bufferIn[byteNum++];
+        byteNum++;
+
+        // Get the map name
+        while( bufferIn[byteNum] != 0x00 ) map += (char)bufferIn[byteNum++];
+        byteNum++;
+
+        // Skip the game server path
+        while( bufferIn[byteNum] != 0x00 ) byteNum++;
+        byteNum++;
+
+        // Get the game description
+        while( bufferIn[byteNum] != 0x00 ) game += (char)bufferIn[byteNum++];
+        byteNum++;
+
+        // Get the current number of players and move to the next byte
+        numPlayers = (int)bufferIn[byteNum++];
+
+        // Get the maximum number of players and move to the next byte
+        maxPlayers = (int)bufferIn[byteNum++];
+        
+        result.putString(Values.SERVER_NAME, name);
+        result.putString(Values.SERVER_MAP, map);
+        result.putString(Values.SERVER_GAME, game);
+        result.putString(Values.SERVER_VERSION, version);
+        result.putString(Values.SERVER_TAGS, tags);
+        result.putInt(Values.SERVER_NUM_PLAYERS, numPlayers);
+        result.putInt(Values.SERVER_MAX_PLAYERS, maxPlayers);
+        
+        return result;
     }
 }
