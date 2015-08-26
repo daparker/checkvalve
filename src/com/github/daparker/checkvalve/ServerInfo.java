c82d25f8fb5b51458006d9322140ee45385be95a
David A. Parker <parker.david.a@gmail.com>
2015-08-26 11:56:59 -0400

    Added gane, port, listPos and rowId
    Also added several get and set methods

--- src/com/github/daparker/checkvalve/ServerInfo.java
+++ src/com/github/daparker/checkvalve/ServerInfo.java
@@ -19,34 +19,109 @@
 
 package com.github.daparker.checkvalve;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /*
  * Define the ServerInfo class
  */
-public class ServerInfo {
+public class ServerInfo implements Parcelable {
     private String name;
     private String addr;
+    private String game;
     private String version;
     private String map;
     private String tags;
+    private int port;
     private int numPlayers;
     private int maxPlayers;
+    private int listPos;
+    private long rowId;
 
-    public ServerInfo( String name, String addr, String version, String map, String tags, int numPlayers, int maxPlayers ) {
+    public ServerInfo() {}
+
+    /**
+     * Create a new ServerInfo object using information from an A2S_INFO response
+     * 
+     * @param name The server's name
+     * @param addr The server's IP address
+     * @param game The game which is running on the server
+     * @param version The version of the game
+     * @param map The current map
+     * @param tags The server's tags
+     * @param port The listen port of the server
+     * @param numPlayers The number of players on the server
+     * @param maxPlayers The max number of players the server will support
+     */
+    public ServerInfo( String name, String addr, String game, String version, String map, String tags,
+            int port, int numPlayers, int maxPlayers, int listPos, long rowId ) {
         this.name = name;
         this.addr = addr;
+        this.game = game;
         this.version = version;
         this.map = map;
         this.tags = tags;
+        this.port = port;
         this.numPlayers = numPlayers;
         this.maxPlayers = maxPlayers;
+        this.listPos = listPos;
+        this.rowId = rowId;
     }
 
+    public void setName( String s ) {
+        this.name = s;
+    }
+    
+    public void setAddress( String s ) {
+        this.addr = s;
+    }
+    
+    public void setGame( String s ) {
+        this.game = s;
+    }
+    
+    public void setVersion( String s ) {
+        this.version = s;
+    }
+    
+    public void setMap( String s ) {
+        this.map = s;
+    }
+    
+    public void setTags( String s ) {
+        this.tags = s;
+    }
+    
+    public void setPort( int i ) {
+        this.port = i;
+    }
+    
+    public void setNumPlayers( int i ) {
+        this.numPlayers = i;
+    }
+    
+    public void setListPos( int i ) {
+        this.listPos = i;
+    }
+    
+    public void setRowId( long l ) {
+        this.rowId = l;
+    }
+    
+    public void setMaxPlayers( int i ) {
+        this.maxPlayers = i;
+    }
+    
     public String getName() {
         return this.name;
     }
 
     public String getAddr() {
         return this.addr;
+    }
+    
+    public String getGame() {
+        return this.game;
     }
 
     public String getVersion() {
@@ -60,6 +135,10 @@ public class ServerInfo {
     public String getTags() {
         return this.tags;
     }
+    
+    public int getPort() {
+        return this.port;
+    }
 
     public int getNumPlayers() {
         return this.numPlayers;
@@ -67,5 +146,55 @@ public class ServerInfo {
 
     public int getMaxPlayers() {
         return this.maxPlayers;
+    }
+    
+    public int getListPos() {
+        return this.listPos;
+    }
+    
+    public long getRowId() {
+        return this.rowId;
+    }
+    
+    public int describeContents() {
+        return 0;
+    }
+
+    public void writeToParcel( Parcel dest, int flags ) {
+        dest.writeString(this.name);
+        dest.writeString(this.addr);
+        dest.writeString(this.game);
+        dest.writeString(this.version);
+        dest.writeString(this.map);
+        dest.writeString(this.tags);
+        dest.writeInt(this.port);
+        dest.writeInt(this.numPlayers);
+        dest.writeInt(this.maxPlayers);
+        dest.writeInt(this.listPos);
+        dest.writeLong(this.rowId);
+    }
+
+    public static final Parcelable.Creator<ServerInfo> CREATOR = new Parcelable.Creator<ServerInfo>() {
+        public ServerInfo createFromParcel( Parcel in ) {
+            return new ServerInfo(in);
+        }
+
+        public ServerInfo[] newArray( int size ) {
+            return new ServerInfo[size];
+        }
+    };
+
+    private ServerInfo( Parcel in ) {
+        name = in.readString();
+        addr = in.readString();
+        game = in.readString();
+        version = in.readString();
+        map = in.readString();
+        tags = in.readString();
+        port = in.readInt();
+        numPlayers = in.readInt();
+        maxPlayers = in.readInt();
+        listPos = in.readInt();
+        rowId = in.readLong();
     }
 }
