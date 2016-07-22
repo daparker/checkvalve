0533ec04b8cd635c6edf81909e38ed1dae1f7a90
David A. Parker <parker.david.a@gmail.com>
2016-07-22 16:17:31 -0400

    Added showNote() method

--- src/com/github/daparker/checkvalve/UserVisibleMessage.java
+++ src/com/github/daparker/checkvalve/UserVisibleMessage.java
@@ -19,7 +19,9 @@
 
 package com.github.daparker.checkvalve;
 
+import java.io.File;
 import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -57,5 +59,31 @@ public class UserVisibleMessage {
         Toast t = Toast.makeText(context, resId, Toast.LENGTH_SHORT);
         t.show();
         return t;
+    }
+    
+    /**
+     * Displays a note to the user and includes the option to not show this note again.
+     * 
+     * @param context The context to use
+     * @param file The filename of the marker file to create or delete
+     * @param resId The resource ID of the message to display in the note
+     */
+    public static void showNote( Context context, String file, int resId ) {
+        File f = new File(context.getFilesDir(), file);
+        
+        if( f.exists() ) {
+            Log.d(TAG, "Marker file " + file + " exists; note will not be displayed.");
+            return;
+        }
+        
+        Log.d(TAG, "Marker file " + file + " does not exist; showing note.");
+        
+        
+        Intent showNoteIntent = new Intent();
+        showNoteIntent.setClassName("com.github.daparker.checkvalve", "com.github.daparker.checkvalve.ShowNoteActivity");
+        showNoteIntent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+        showNoteIntent.putExtra(Values.EXTRA_NOTE_ID, resId);
+        showNoteIntent.putExtra(Values.EXTRA_FILE_NAME, file);
+        context.startActivity(showNoteIntent);
     }
 }
