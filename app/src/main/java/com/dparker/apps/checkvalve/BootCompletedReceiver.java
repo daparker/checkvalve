81491189c0edb2ab4f93100c7b269128867dc017
David A. Parker <parker.david.a@gmail.com>
2019-10-09 15:18:42 -0400

    Version 2.0.10-stable
    Use scheduled job instead of service (API 21+)
    
    Code cleanup

--- app/src/main/java/com/dparker/apps/checkvalve/BootCompletedReceiver.java
+++ app/src/main/java/com/dparker/apps/checkvalve/BootCompletedReceiver.java
@@ -29,30 +29,33 @@ public class BootCompletedReceiver extends BroadcastReceiver {
     final static String TAG = BootCompletedReceiver.class.getSimpleName();
 
     @Override
-    public void onReceive(Context context, Intent arg1) {
-        if( android.os.Build.VERSION.SDK_INT >= 11 ) {
-            DatabaseProvider db = new DatabaseProvider(context);
+    public void onReceive(Context context, Intent intent) {
+        if( Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction()) ) {
+            if( android.os.Build.VERSION.SDK_INT >= 21 ) {
+                Log.d(TAG, "[CHECKVALVE]: SDK >= 21; calling BackgroundJobUtil.scheduleJob()");
+                BackgroundJobUtil.scheduleJob(context, true);
+            }
+            else {
+                DatabaseProvider db = new DatabaseProvider(context);
 
-            try {
-                boolean enabled = db.getBooleanSetting(DatabaseProvider.SETTINGS_ENABLE_NOTIFICATIONS);
+                try {
+                    boolean enabled = db.getBooleanSetting(DatabaseProvider.SETTINGS_ENABLE_NOTIFICATIONS);
 
-                if( enabled ) {
-                    Log.d(TAG, "[CHECKVALVE] Starting background query service.");
-                    context.startService(new Intent(context, BackgroundQueryService.class));
+                    if( enabled ) {
+                        Log.d(TAG, "[CHECKVALVE] Starting background query service.");
+                        context.startService(new Intent(context, BackgroundQueryService.class));
+                    }
+                    else {
+                        Log.d(TAG, "[CHECKVALVE] Background service is disabled in settings.");
+                    }
                 }
-                else {
-                    Log.d(TAG, "[CHECKVALVE] Background service is disbaled in settings.");
+                catch( Exception e ) {
+                    Log.w(TAG, "[CHECKVALVE] Caught an exception:", e);
+                }
+                finally {
+                    db.close();
                 }
             }
-            catch( Exception e ) {
-                Log.w(TAG, "[CHECKVALVE] Caught an exception:", e);
-            }
-            finally {
-                db.close();
-            }
-        }
-        else {
-            Log.e(TAG, "[CHECKVALVE] Background service requires Android 3.0 or higher.");
         }
     }
 }
